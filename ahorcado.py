2ef16ae37d7e7e015241d71ca5d04fea87540f8b
Carlos Gomez <frioold30@gmail.com>
2022-08-08 17:44:20 -0500

    Agregada funcion que quita salto de linea y ciclo para adivinar la palabra

--- ahorcado.py
+++ ahorcado.py
@@ -5,11 +5,19 @@ def words():
     word=[]
     with open(r"C:\Users\ADMIN\Desktop\PLATZI\CURSO_INTERMEDIO_PYTHON\final_project\data.txt", "r",encoding="utf-8") as f:
         for line in f:
-            word.append(str(line))
-            
-    pala = random.choice(word)
+            line = line.rstrip("\n")
+            word.append((line))     #tengo que solucionar el problema de que las palabras las guarda con un \n
+    print(word)        
+    # pala = random.choice(word)
+    # letter = input("Escribe una letra: ")
+    # while letter != pala:
+    #     if letter in pala:
+    #         print(letter)
+    #         letter = input("Bien!, Escoje otra letra: ")
+    #     else:
+    #         letter = input("Escoje otra letra: ")
     
-    print(pala)
+
         
 def run():
     words()
