e9f04a9bd6baae3a7ad1053cd6dbe91b10ff77a7
Carlos Gomez <frioold30@gmail.com>
2022-08-11 22:41:36 -0500

    Terminada la version funcional del juego

--- ahorcado.py
+++ ahorcado.py
@@ -6,21 +6,37 @@ def words():
     with open(r"C:\Users\ADMIN\Desktop\PLATZI\CURSO_INTERMEDIO_PYTHON\final_project\data.txt", "r",encoding="utf-8") as f:
         for line in f:
             line = line.rstrip("\n")
-            word.append((line))     #tengo que solucionar el problema de que las palabras las guarda con un \n
-    print(word)        
-    # pala = random.choice(word)
-    # letter = input("Escribe una letra: ")
-    # while letter != pala:
-    #     if letter in pala:
-    #         print(letter)
-    #         letter = input("Bien!, Escoje otra letra: ")
-    #     else:
-    #         letter = input("Escoje otra letra: ")
+            word.append((line))    
+         
+    pala = random.choice(word)
+    lst = []
+    guion = "_"
+    spaces = len(pala)
+    lst.append(guion*spaces)
+    lst = "".join(lst)
+    letter = input("Escribe una letra: ")
+    while letter != pala:
+        inicio = 0
+        final = 1
+        for i in pala:
+            if i == letter:
+                lst = lst[:inicio] + letter + lst[final:]
+                inicio +=1
+                final += 1
+            else:
+                inicio +=1
+                final += 1
+                continue
+        print(lst)
+        letter = input("Escibre otra letra: ")
     
+    print(pala)
 
-        
+
+       
 def run():
     words()
+    print("ENHORABUENA! HAS GANADO :D")
 
 
 if __name__ == "__main__":
