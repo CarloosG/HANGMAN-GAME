9dc0d5d60b9c4096bd3b27dcf82870ee2beaa6c5
Carlos Gomez <frioold30@gmail.com>
2022-08-18 17:30:12 -0500

    Terminado todo el juego del ahorcado

--- ahorcado.py
+++ ahorcado.py
@@ -1,29 +1,29 @@
 import random
+import os 
 
 
 def words():
-    word=[]
+    words=[]
     with open(r"C:\Users\ADMIN\Desktop\PLATZI\CURSO_INTERMEDIO_PYTHON\final_project\data.txt", "r",encoding="utf-8") as f:
         for line in f:
             line = line.rstrip("\n")
-            word.append((line))    
-         
-    # pala = random.choice(word)
-    pala = "solución"
+            words.append((line))    
+    selected_word = random.choice(words)
     lst = []
     guion = "_"
-    spaces = len(pala)
-    lst.append(guion*spaces)
+    blanks = len(selected_word)
+    lst.append(guion*blanks)
     lst = "".join(lst)
-    vidas = 15
-    print("Tienes " + str(vidas) + " vidas.Buenas suerte!")
+    lives = 10
+    print("Tienes " + str(lives) + " vidas.Buena suerte!")
     print(lst)
     letter = input("Escribe una letra: ")
+    letter = letter.lower()
 
-    while letter != pala:
+    while letter != selected_word:
         inicio = 0
         final = 1
-        for i in pala:
+        for i in selected_word:
             if i == letter:
                 lst = lst[:inicio] + letter + lst[final:]
                 inicio +=1
@@ -31,22 +31,55 @@ def words():
             else:
                 inicio +=1
                 final += 1
+                lives = (lives-1)
                 continue
-        vidas = vidas-1
-        print("Te quedan " + str(vidas) + " vidas")
-        if vidas == 0:
+        lives = (lives + (blanks-1))
+        os.system("cls")
+        print("Te quedan " + str(lives) + " vidas")
+        if lives == 9:
+            print(cuerda)
+        if lives == 8:
+            print(head)
+        if lives == 7:
+            print(body)
+        if lives == 6:
+            print(arm_1)
+        if lives == 5:
+            print(arm_2)
+        if lives == 4:
+            print(leg_1)
+        if lives == 3:
+            print(leg_2)
+        if lives == 2:
+            print(eye_1)
+        if lives == 1:
+            print(eye_2)                  
+        if lives == 0:
+            print(mouth)
             print(lose)
-            print("La palabra era " + pala + " :(")
+            print("La palabra era " + selected_word + " :(")
             break
         print(lst)
         letter = input("Escribe otra letra: ")
-    if letter == pala:
+        letter = letter.lower()
+    if letter == selected_word:
                 print(win)
-                print("Muy bienn, la palabra era " + pala + " :D")
+                print("Muy bienn, la palabra era " + selected_word + " :D")
 
 
 
-def instrucciones():
+start="""
+    -- -- -- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+    
+    Bienvenido al juego del ahorcado
+    Escoje S para empezar
+    Escoje I para leer las instrucciones 
+    Escoje E para salir
+
+    -- -- -- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+"""
+
+def instructions():
     print("""
     -- -- -- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
     
@@ -55,7 +88,7 @@ def instrucciones():
     El juego del ahorcado es muy sencillo!Se mostraran espacios
     En cada uno de estos espacios va una letra
     Estas letras conforman una palabra que tendras que adivinar
-    Solo puedes usar letras y una misma letra puede estar en varios espacios de la palabra
+    Solo puedes usar letras, algunas letras llevan tilde y una misma letra puede estar en varios espacios de la palabra
 
     !!!Si pierdes todas tus vidas seras ahorcado!!!
 
@@ -116,12 +149,12 @@ win = """
  |_|_| |____| | | | | | | (_) | | | (_| | |_) | |_| |  __/ | | | (_| |_|_|      
  (_|_)______|_| |_|_| |_|\___/|_|  \__,_|_.__/ \__,_|\___|_| |_|\__,_(_|_)
  
-                                                                        ____  
- | |  | |                                                   | |        _|  __ \ 
- | |__| | __ _ ___    __ _  __ _ _ __   __ _ _ __   __ _  __| | ___   (_) |  | |
- |  __  |/ _` / __|  / _` |/ _` | '_ \ / _` | '_ \ / _` |/ _` |/ _ \    | |  | |
- | |  | | (_| \__ \ | (_| | (_| | | | | (_| | | | | (_| | (_| | (_) |  _| |__| |
- |_|  |_|\__,_|___/  \__, |\__,_|_| |_|\__,_|_| |_|\__,_|\__,_|\___/  (_)_____/ 
+                                                 _  
+ | |  | |                                       | |        _|  __ \ 
+ | |__| | __ _ ___    __ _  __ _ _ __   __ _  __| | ___   (_) |  | |
+ |  __  |/ _` / __|  / _` |/ _` | '_ \ / _` |/ _` |/ _ \    | |  | |
+ | |  | | (_| \__ \ | (_| | (_| | | | | (_| | (_| | (_) |  _| |__| |
+ |_|  |_|\__,_|___/  \__, |\__,_|_| |_|\__,_|\__,_|\___/  (_)_____/ 
                       __/ |                                                     
                      |___/   
 
@@ -141,67 +174,302 @@ lose = """
 """
 
 cuerda = """
-  _                                    
+  _                                _ 
+ | |___ ___ ___ ___ ___ ___ ____ _| |
+ | |___|___|___|___|___|___|____|_| |
+ | |                              | |
+ |_|                              |_|
+ | |                             
+ | |                                          
+ | |                        
+ |_|                         
+ | |                          
+ | |                            
+ | |                                    
+ |_|                                  
+ | |                              
  | |                                   
- | |______ ______ ______ ______ ______ 
- | |______|______|______|______|______|
- | |                                   
- | |                                   
- | |                                   
- |_|                                   
- | |                                   
- | |                                   
- | |                                   
- | |                                   
- | |                                   
- | |                                   
- |_|                                   
- | |                                   
- | |                                   
- | |                                   
- | |                                   
- | |                                   
- | |                                   
- |_|                                   
- | |                                   
- | |                                   
- | |                                   
- | |                                   
- | |                                   
- | |                                   
- |_|                                   
- | |                                   
- | |                                   
- | |                                   
- | |                                   
- | |                                   
- | |                                   
+ | |                                  
+ |_|                                  
+ | |                                  
+ | |                                  
+ | |                                  
+ |_|                                  
+ | |                                  
+ | |                                  
+ | |                                  
+ |_|                              
+                                
  
 """
 head = """
-        *  *            
-     *        *            
-    *          *           
-    *          *             
-     *        *             
-        *  *      
+  _                                _ 
+ | |___ ___ ___ ___ ___ ___ ____ _| |
+ | |___|___|___|___|___|___|____|_| |
+ | |                              | |
+ |_|                              |_|
+ | |                             *   *  
+ | |                          *         *                  
+ | |                         *           *
+ |_|                         *           *
+ | |                          *         *
+ | |                             *   *
+ | |                                    
+ |_|                                  
+ | |                              
+ | |                                   
+ | |                                  
+ |_|                                  
+ | |                                  
+ | |                                  
+ | |                                  
+ |_|                                  
+ | |                                  
+ | |                                  
+ | |                                  
+ |_|                              
+          
+"""
+body = """
+  _                                _ 
+ | |___ ___ ___ ___ ___ ___ ____ _| |
+ | |___|___|___|___|___|___|____|_| |
+ | |                              | |
+ |_|                              |_|
+ | |                             *   *  
+ | |                          *         *                  
+ | |                         *           *
+ |_|                         *           *
+ | |                          *         *
+ | |                             *   *
+ | |                               *     
+ |_|                               *   
+ | |                               *
+ | |                               *   
+ | |                               *  
+ |_|                               *  
+ | |                               *  
+ | |                               *  
+ | |                               *   
+ |_|                                  
+ | |                                  
+ | |                                  
+ | |                                  
+ |_|     
+
 """
 
+arm_1 = """
+  _                                _ 
+ | |___ ___ ___ ___ ___ ___ ____ _| |
+ | |___|___|___|___|___|___|____|_| |
+ | |                              | |
+ |_|                              |_|
+ | |                             *   *  
+ | |                          *         *                  
+ | |                         *           *
+ |_|                         *           *
+ | |                          *         *
+ | |                             *   *
+ | |                               *     
+ |_|                             * *   
+ | |                           *   *
+ | |                         *     *   
+ | |                       *       *  
+ |_|                               *  
+ | |                               *  
+ | |                               *  
+ | |                               *   
+ |_|                                  
+ | |                                  
+ | |                                  
+ | |                                  
+ |_|     
 
+"""
+arm_2 = """
+  _                                _ 
+ | |___ ___ ___ ___ ___ ___ ____ _| |
+ | |___|___|___|___|___|___|____|_| |
+ | |                              | |
+ |_|                              |_|
+ | |                             *   *  
+ | |                          *         *                  
+ | |                         *           *
+ |_|                         *           *
+ | |                          *         *
+ | |                             *   *
+ | |                               *     
+ |_|                             * * *  
+ | |                           *   *   *
+ | |                         *     *     *
+ | |                       *       *       *
+ |_|                               *  
+ | |                               *  
+ | |                               *  
+ | |                               *   
+ |_|                                  
+ | |                                  
+ | |                                  
+ | |                                  
+ |_|     
+
+"""
+leg_1 = """
+  _                                _ 
+ | |___ ___ ___ ___ ___ ___ ____ _| |
+ | |___|___|___|___|___|___|____|_| |
+ | |                              | |
+ |_|                              |_|
+ | |                             *   *  
+ | |                          *         *                  
+ | |                         *           *
+ |_|                         *           *
+ | |                          *         *
+ | |                             *   *
+ | |                               *     
+ |_|                             * * *  
+ | |                           *   *   *
+ | |                         *     *     *
+ | |                       *       *       *
+ |_|                               *  
+ | |                               *  
+ | |                               *  
+ | |                               *   
+ |_|                              *    
+ | |                             *     
+ | |                            *      
+ | |                           *       
+ |_|                          *
+
+"""
+leg_2 = """
+  _                                _ 
+ | |___ ___ ___ ___ ___ ___ ____ _| |
+ | |___|___|___|___|___|___|____|_| |
+ | |                              | |
+ |_|                              |_|
+ | |                             *   *  
+ | |                          *         *                  
+ | |                         *           *
+ |_|                         *           *
+ | |                          *         *
+ | |                             *   *
+ | |                               *     
+ |_|                             * * *  
+ | |                           *   *   *
+ | |                         *     *     *
+ | |                       *       *       *
+ |_|                               *  
+ | |                               *  
+ | |                               *  
+ | |                               *   
+ |_|                              * *  
+ | |                             *   *  
+ | |                            *     * 
+ | |                           *       *
+ |_|                          *         *
+
+"""
+eye_1 = """
+  _                                _ 
+ | |___ ___ ___ ___ ___ ___ ____ _| |
+ | |___|___|___|___|___|___|____|_| |
+ | |                              | |
+ |_|                              |_|
+ | |                             *   *  
+ | |                          * \/      *                  
+ | |                         *  /\       *
+ |_|                         *           *
+ | |                          *         *
+ | |                             *   *
+ | |                               *     
+ |_|                             * * *  
+ | |                           *   *   *
+ | |                         *     *     *
+ | |                       *       *       *
+ |_|                               *  
+ | |                               *  
+ | |                               *  
+ | |                               *   
+ |_|                              * *  
+ | |                             *   *  
+ | |                            *     * 
+ | |                           *       *
+ |_|                          *         *
+
+"""
+eye_2 = """
+  _                                _ 
+ | |___ ___ ___ ___ ___ ___ ____ _| |
+ | |___|___|___|___|___|___|____|_| |
+ | |                              | |
+ |_|                              |_|
+ | |                             *   *  
+ | |                          * \/   \/ *                  
+ | |                         *  /\   /\  *
+ |_|                         *           *
+ | |                          *         *
+ | |                             *   *
+ | |                               *     
+ |_|                             * * *  
+ | |                           *   *   *
+ | |                         *     *     *
+ | |                       *       *       *
+ |_|                               *  
+ | |                               *  
+ | |                               *  
+ | |                               *   
+ |_|                              * *  
+ | |                             *   *  
+ | |                            *     * 
+ | |                           *       *
+ |_|                          *         *
+
+"""
+mouth = """
+  _                                _ 
+ | |___ ___ ___ ___ ___ ___ ____ _| |
+ | |___|___|___|___|___|___|____|_| |
+ | |                              | |
+ |_|                              |_|
+ | |                             *   *  
+ | |                          * \/   \/ *                  
+ | |                         *  /\   /\  *
+ |_|                         *    _ _    *
+ | |                          *  /   \  *
+ | |                             *   *
+ | |                               *     
+ |_|                             * * *  
+ | |                           *   *   *
+ | |                         *     *     *
+ | |                       *       *       *
+ |_|                               *  
+ | |                               *  
+ | |                               *  
+ | |                               *   
+ |_|                              * *  
+ | |                             *   *  
+ | |                            *     * 
+ | |                           *       *
+ |_|                          *         *
+
+"""
 
 
 def run():
     print(menu)
     while True:
-            choise = input("Bienvenido al juego del ahorcado\n Escoje S para empezar\n Escoje I para leer las instrucciones \n Escoje E para salir\n ")
+            choise = input(start)
+            os.system("cls")
             if choise == "S":
                 words()
                 break
             elif choise == "E":
-                print("Thanks for playing")
+                print("Gracias por jugar")
                 break
             elif choise == "I":
-                instrucciones()
+                instructions()
             else:
                 print("Debes escoger una opcion valida\n")
                 continue
